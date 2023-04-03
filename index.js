eb66e3d9ad59cd983729ff4fab56949a8be885dd
Maryanne Wambugu <2wambugu.marie@gmail.com>
2023-04-03 17:29:13 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution here!
+ const cats = ["Milo", "Otis", "Garfield"];
+ function destructivelyAppendCat(name) {
+    return cats.push(name);
+ }
+ function destructivelyPrependCat(name) {
+    return cats.unshift(name);
+ }
+ function destructivelyRemoveLastCat() {
+    cats.pop();
+    return cats;
+ }
+ function destructivelyRemoveFirstCat() {
+    cats.shift();
+    return cats;
+ }
+ function appendCat(name) {
+    const appends = [...cats,name];
+    return appends;
+ }
+ function prependCat(name) {
+    const prepends = [name,...cats];
+    return prepends;
+ }
+ function removeLastCat() {
+    return cats.slice(0,-1);
+ }
+ function removeFirstCat() {
+    return cats.slice(1);
+ }
